ee93de01ed0f247a849943d3680f4abfb1293248
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-10-04 16:52:58 +1300

    better column identification algorithm

--- load_nzgd_data_script.py
+++ load_nzgd_data_script.py
@@ -1,3 +1,6 @@
+"""
+Script to load NZGD data and convert to a standard format
+"""
 import numpy as np
 from pathlib import Path
 import toml
@@ -6,37 +9,56 @@ import xlrd
 from tqdm import tqdm
 import natsort
 
-parquet_output_dir = Path("/home/arr65/data/nzgd/standard_format/cpt/data")
-metadata_output_dir = Path("/home/arr65/data/nzgd/standard_format/cpt/metadata")
+output_dir = Path("/home/arr65/data/nzgd/standard_format_batch1/cpt")
+
+parquet_output_dir = output_dir / "data"
+metadata_output_dir = output_dir / "metadata"
 
 parquet_output_dir.mkdir(exist_ok=True, parents=True)
 metadata_output_dir.mkdir(exist_ok=True, parents=True)
-
-# Define possible names for each column
-possible_xls_cols = [
-    ["Depth [m]", "Depth", "Depth (m)"],
-    ["Cone resistance (qc) in MPa", "qc [MPa]",  "Tip resistance", "qc Clean (MPa)", "Qc [MPa]"],
-    ["Sleeve friction (fs) in MPa", "fs [MPa]", "Local friction","fs Clean (MPa)", "Fs [KPa]"],
-    ["Tip resistance", "Local friction", "Pore shoulder","u2 Clean (MPa)", "Dynamic pore pressure (u2) in MPa", "u2 [MPa]", "U2 [KPa]"]
-]
-
 
 downloaded_files = Path("/home/arr65/data/nzgd/downloaded_files/cpt")
 
 meta_successfully_loaded = []
 meta_failed_to_load = []
+meta_ags_failed_to_load = []
+meta_xls_failed_to_load = []
 
 record_counter = 0
 
-for record_dir in tqdm(natsort.natsorted(list(downloaded_files.glob("*")))):
-#for record_dir in tqdm(natsort.natsorted(list(downloaded_files.glob("*")))[0:100]):
-#for record_dir in [Path("/home/arr65/data/nzgd/downloaded_files/cpt/CPT_1")]:
-#for record_dir in failed_ags_loads:
+#previously_converted_filenames = list(np.loadtxt("/home/arr65/data/nzgd/standard_format_batch1/cpt/metadata/successfully_loaded.txt", dtype=str))
+previously_converted_filenames = []
+
+previously_converted_records = []
+for filename in previously_converted_filenames:
+    file_name_parts = filename.split("_")
+    previously_converted_records.append(f"{file_name_parts[0]}_{file_name_parts[1]}")
+
+#records_to_convert = [x for x in list(downloaded_files.glob("*")) if x.name not in previously_converted_records]
+records_to_convert = []
+for record_dir in natsort.natsorted(list(downloaded_files.glob("*"))):
+    if record_dir.name not in previously_converted_records:
+        records_to_convert.append(record_dir)
+
+for record_dir in tqdm(records_to_convert):
+#for record_dir in [Path("/home/arr65/data/nzgd/downloaded_files/cpt/CPT_12402")]:
+
+    ags_file_load_attempted = False
+    xls_file_load_attempted = False
+
+    ags_load_failed = False
+    xls_load_failed = False
 
     record_counter += 1
     if record_counter % 100 == 0:
-        np.savetxt(metadata_output_dir / "successfully_loaded.txt", np.array(meta_successfully_loaded), fmt="%s")
-        np.savetxt(metadata_output_dir / "failed_to_load.txt", np.array(meta_failed_to_load), fmt="%s")
+        np.savetxt(metadata_output_dir / "successfully_loaded.txt", np.array(meta_successfully_loaded), fmt="%s",
+                   header="successfully_loaded_files")
+        np.savetxt(metadata_output_dir / "failed_to_load.txt", np.array(meta_failed_to_load), fmt="%s",
+                   header="record_name, file_name, error_message")
+        np.savetxt(metadata_output_dir / "ags_failed_to_load.txt", np.array(meta_ags_failed_to_load), fmt="%s",
+                   header="record_name, file_name, error_message")
+        np.savetxt(metadata_output_dir / "xls_failed_to_load.txt", np.array(meta_xls_failed_to_load), fmt="%s",
+                   header="record_name, file_name, error_message")
 
     has_loaded_a_file_for_this_record = False
 
@@ -45,7 +67,10 @@ for record_dir in tqdm(natsort.natsorted(list(downloaded_files.glob("*")))):
     if len(files_to_try) > 0:
         for file_to_try in files_to_try:
             try:
+                ags_file_load_attempted = True
                 record_df = loading_funcs_for_nzgd_data.load_ags(file_to_try)
+                record_df.insert(0,"record_name",record_dir.name)
+                record_df.reset_index(inplace=True, drop=True)
                 record_df.attrs["original_file_name"] = file_to_try.name
                 record_df.to_parquet(parquet_output_dir / f"{record_dir.name}.parquet")
                 meta_successfully_loaded.append(file_to_try.name)
@@ -55,7 +80,8 @@ for record_dir in tqdm(natsort.natsorted(list(downloaded_files.glob("*")))):
 
             ## If the ags file is missing data, KeyError or UnboundLocalError will be raised
             except(KeyError, UnboundLocalError) as e:
-                meta_failed_to_load.append(f"{record_dir.name}, {file_to_try.name}, {str(e)}")
+                meta_ags_failed_to_load.append(f"{record_dir.name}, {file_to_try.name}, {str(e).replace(',',';')}")
+                ags_load_failed = True
                 pass
 
     if has_loaded_a_file_for_this_record:
@@ -68,18 +94,37 @@ for record_dir in tqdm(natsort.natsorted(list(downloaded_files.glob("*")))):
     if len(files_to_try) > 0:
         for file_to_try in files_to_try:
             try:
-                record_df = loading_funcs_for_nzgd_data.load_xls_file_brute_force(file_to_try, possible_xls_cols)
+                xls_file_load_attempted = True
+                record_df = loading_funcs_for_nzgd_data.load_cpt_xls_file(file_to_try)
                 record_df.attrs["original_file_name"] = file_to_try.name
+                record_df.insert(0,"record_name",record_dir.name)
+                record_df.reset_index(inplace=True, drop=True)
                 record_df.to_parquet(parquet_output_dir / f"{record_dir.name}.parquet")
                 meta_successfully_loaded.append(file_to_try.name)
                 has_loaded_a_file_for_this_record = True
                 continue
+
             except(ValueError, xlrd.compdoc.CompDocError, Exception) as e:
-                meta_failed_to_load.append(f"{record_dir.name}, {file_to_try.name}, {e}")
+                meta_xls_failed_to_load.append(f"{record_dir.name}, {file_to_try.name}, {e}")
                 pass
+                xls_load_failed = True
 
-    if not has_loaded_a_file_for_this_record:
-        meta_failed_to_load.append(f"{record_dir.name}, Did_not_attempt_to_load_any_files, ")
+    if (not ags_file_load_attempted) and (not xls_file_load_attempted):
+        meta_failed_to_load.append(f"{record_dir.name}, N/A, Did_not_attempt_to_load_any_files")
 
-np.savetxt(metadata_output_dir / "successfully_loaded.txt", np.array(meta_successfully_loaded), fmt="%s")
-np.savetxt(metadata_output_dir / "failed_to_load.txt", np.array(meta_failed_to_load), fmt="%s")
+    if ags_load_failed and xls_load_failed:
+        meta_failed_to_load.append(f"{record_dir.name}, N/A, Both_ags_and_xls_load_failed")
+
+    if ags_load_failed and (not xls_file_load_attempted):
+        meta_failed_to_load.append(f"{record_dir.name}, {file_to_try.name}, ags_load_failed")
+
+    if xls_load_failed and (not ags_file_load_attempted):
+        meta_failed_to_load.append(f"{record_dir.name}, {file_to_try.name}, xls_load_failed")
+
+
+
+
+np.savetxt(metadata_output_dir / "successfully_loaded.txt", np.array(meta_successfully_loaded), fmt="%s",header="successfully_loaded_files")
+np.savetxt(metadata_output_dir / "failed_to_load.txt", np.array(meta_failed_to_load), fmt="%s",header="record_name, file_name, error_message")
+np.savetxt(metadata_output_dir / "ags_failed_to_load.txt", np.array(meta_ags_failed_to_load), fmt="%s",header="record_name, file_name, error_message")
+np.savetxt(metadata_output_dir / "xls_failed_to_load.txt", np.array(meta_xls_failed_to_load), fmt="%s",header="record_name, file_name, error_message")
--- loading_funcs_for_nzgd_data.py
+++ loading_funcs_for_nzgd_data.py
@@ -7,8 +7,85 @@ from typing import Union
 from pathlib import Path
 import pandas as pd
 from itertools import product
+import numpy as np
+import scipy
+import copy
+ #scipy.stats import mode
 
-def load_ags(file_path: Union[Path, str]):
+def find_col_name_from_substring(df:pd.DataFrame,
+                                 substrings:list[str],
+                                 remaining_cols_to_search:list[str],
+                                 target_column_name:str) -> tuple[str, pd.DataFrame, list[str]]:
+
+    """
+    Find a column name containing a substring in a DataFrame.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        A dataframe containing the loaded xls file.
+    substrings : list[str]
+        A list of substrings to search for in the column names.
+    remaining_cols_to_search : list[str]
+        A list of column names to search for the substring in.
+    target_column_name : str
+        The name of the column being searched for.
+
+    Returns
+    -------
+    tuple[str, pd.DataFrame, list[str]]
+        The name of the column containing the substring, the updated DataFrame,
+        and the updated list of columns to search.
+    """
+
+    candidate_col_names = []
+    for col_name in remaining_cols_to_search:
+        if isinstance(col_name, str):
+            for substring in substrings:
+                if substring in col_name.lower():
+                    if col_name not in candidate_col_names:
+                        candidate_col_names.append(col_name)
+
+    if len(candidate_col_names) >= 1:
+        # check for "Clean" which is sometimes used for a cleaned version of the same data
+        if len(candidate_col_names) > 1:
+            col = None
+            for candidate_name in candidate_col_names:
+                if "clean" in candidate_name.lower():
+                    col = candidate_name
+                    break
+            if col is None:
+                col = candidate_col_names[0]
+
+
+        else:
+            col = candidate_col_names[0]
+
+        df.attrs[f"candidate_{target_column_name}_column_names_in_original_file"] = candidate_col_names
+        df.attrs[f"adopted_{target_column_name}_column_name_in_original_file"] = col
+        remaining_cols_to_search.remove(col)
+
+        if target_column_name != "depth":
+            # if the column is in kPa, convert to MPa
+            if "kpa" in col.lower():
+                df.loc[:, col] /= 1000
+        if target_column_name == "depth":
+            # if the depth column is in cm, convert to m
+            if "cm" in col:
+                df.loc[:, col] /= 100
+
+    # no relevant columns were found
+    elif len(candidate_col_names) == 0:
+        col = None
+        if "missing_columns" not in df.attrs:
+            df.attrs["missing_columns"] = [target_column_name]
+        else:
+            df.attrs["missing_columns"].append(target_column_name)
+
+    return col, df, remaining_cols_to_search
+
+
+def load_ags(file_path: Union[Path, str]) -> pd.DataFrame:
     """
     Load an AGS file.
 
@@ -35,105 +112,179 @@ def load_ags(file_path: Union[Path, str]):
     return loaded_data_df.apply(pd.to_numeric, errors='coerce').dropna()
 
 
-def load_xls_basic(file_path: Union[Path, str]):
+def load_cpt_xls_file(file_path: Path) -> pd.DataFrame:
     """
-    Load an XLS file.
+    Load the results of a Cone Penetration Test (CPT) from an Excel file.
 
     Parameters
     ----------
-    file_path : Path or str
-        The path to the XLS file.
+    file_path : Path
+        The path to the Excel file.
 
     Returns
     -------
     pandas.DataFrame
-        The CPT data from the XLS file.
+        A DataFrame containing the relevant CPT data columns:
+            depth, cone resistance, sleeve friction, and porewater pressure.
+
+    Raises
+    ------
+    ValueError
+        If the required columns are not found in any sheet of the Excel file.
     """
-
-    sheet_names = pd.ExcelFile(file_path).sheet_names
-
-    print(sheet_names)
-
-    print()
-
-    if ("REPORT" in sheet_names) and ("Field Sheet" in sheet_names) and ("WORKSHEET" in sheet_names):
-
-        raise IndexError("xls too difficult to load")
-
-    if "Data" in sheet_names:
-        loaded_data_df = pd.read_excel(file_path, sheet_name="Data",header=0)
-
-
-
-        needed_col_names_and_cols = pd.DataFrame({"depth_m": loaded_data_df["Depth [m]"],
-                                                  "qc_mpa": loaded_data_df["Cone resistance (qc) in MPa"],
-                                                  "fs_mpa": loaded_data_df["Sleeve friction (fs) in MPa"],
-                                                  "u_mpa": loaded_data_df["Dynamic pore pressure (u2) in MPa"]})
-
-    elif "CPTU" in sheet_names:
-        loaded_data_df = pd.read_excel(file_path, sheet_name="CPTU",skiprows=32, header=0)
-
-        print()
-
-        needed_col_names_and_cols = pd.DataFrame({"depth_m": loaded_data_df["Depth [m]"],
-                                                  "qc_mpa": loaded_data_df["Qc [MPa]"],
-                                                  "fs_mpa": loaded_data_df["Fs [KPa]"]/1000, ### X/1000 to convert kPa to MPa
-                                                  "u_mpa": loaded_data_df["U2 [KPa]"]})
-
-    elif "CPT SUMMARY SHEET" in sheet_names:
-        loaded_data_df = pd.read_excel(file_path, sheet_name=0, header=0) ## sheet names vary but its always the first sheet
-
-        needed_col_names_and_cols = pd.DataFrame({"depth_m": loaded_data_df["Depth [m]"],
-                                                  "qc_mpa": loaded_data_df["qc [MPa]"],
-                                                  "fs_mpa": loaded_data_df["fs [MPa]"],
-                                                  "u_mpa": loaded_data_df["u2 [MPa]"]})
-
-    elif "-" in sheet_names[0]:
-
-        loaded_data_df = pd.read_excel(file_path, sheet_name=0,
-                                       header=0)  ## sheet names vary but its always the first sheet
-
-        needed_col_names_and_cols = pd.DataFrame({"depth_m": loaded_data_df["Depth"],
-                                                  "qc_mpa": loaded_data_df["Tip resistance"],
-                                                  "fs_mpa": loaded_data_df["Local friction"],
-                                                  "u_mpa": loaded_data_df["Pore shoulder"]})
-
-    return needed_col_names_and_cols
-
-
-def load_xls_file_brute_force(file_path, possible_xls_cols):
-
-    possible_col1 = possible_xls_cols[0]
-    possible_col2 = possible_xls_cols[1]
-    possible_col3 = possible_xls_cols[2]
-    possible_col4 = possible_xls_cols[3]
+    if file_path.suffix.lower() == ".xls":
+        engine = "xlrd"
+    elif file_path.suffix.lower() == ".xlsx":
+        engine = "openpyxl"
+    else:
+        engine = "calamine"
 
     # Load all sheet names in the file
-    sheet_names = pd.ExcelFile(file_path).sheet_names
+    sheet_names = pd.ExcelFile(file_path, engine=engine).sheet_names
+
+    missing_cols_per_sheet = []
 
     # Iterate through each sheet
-    for sheet in sheet_names:
+    for sheet_idx, sheet in enumerate(sheet_names):
 
         # Load the entire sheet without specifying headers
-        df = pd.read_excel(file_path, sheet_name=sheet, header=None)
+        df = pd.read_excel(file_path, sheet_name=sheet, header=None, engine=engine)
+        if df.shape[1] < 4:
+            # There are not enough columns to contain the required data so continue to the next sheet
+            continue
 
-        for idx, row in df.iterrows():
+        df.attrs["original_file_name"] = file_path.name
+        df.attrs["source_sheet_in_original_file"] = sheet
 
-            # Check if the current row contains valid column names
-            for col1, col2, col3, col4 in product(possible_col1, possible_col2, possible_col3, possible_col4):
-                if {col1, col2, col3, col4}.issubset(row):
-                    # Set this row as the header and skip the rows above it
-                    df.columns = row
-                    df = df.iloc[idx + 1:]  # Skip the rows above the header
+        df_float_cell_to_1 = df.map(lambda x: np.nan if isinstance(x, (str)) else 1)
+        num_data_cols = scipy.stats.mode(np.sum(df_float_cell_to_1, axis=1)).mode
 
-                    for col_name in [col2, col3, col4]:
-                        ### Convert KPa to MPa
-                        if "KPa" in col_name:
-                            df.loc[:, col_name] /= 1000
+        df_str_cell_to_1 = df.map(lambda x: np.nan if isinstance(x, (int, float)) else 1)
 
-                    # Return the DataFrame with only the relevant columns
-                    return df[[col1, col2, col3, col4]].rename(
-                        columns={col1: "depth_m", col2: "qc_mpa", col3: "fs_mpa", col4: "u_mpa"})
+        num_text_cells_per_row = np.nansum(df_str_cell_to_1, axis=1)
+        col_name_rows = np.where(num_text_cells_per_row >= num_data_cols)[0]
 
-    # If no valid sheet/column combination was found raise an error
-    raise ValueError(f"Failed to find valid columns in file: {file_path}")
+        # if there are multiple header rows, combine them into one
+        if len(col_name_rows) > 1:
+            col_name_row = np.max(col_name_rows)
+            # copy the column names from the rows above the header row
+            df2 = df.copy()
+            for row_idx in col_name_rows[0:col_name_row]:
+                for col_idx in range(df.shape[1]):
+
+                    # up to but not including the header_row which is taken as the lowest
+                    df2.iloc[col_name_row, col_idx] = str(df.iloc[col_name_row,col_idx]) + " " + str(df.iloc[row_idx,col_idx])
+
+            df = df2.copy()
+
+        # If there is only one header row, take it as the header row
+        else:
+            col_name_row = col_name_rows[0]
+
+
+
+        # set dataframe's headers/column names. Note that .values is used so that the row's index is not included in the header
+        df.columns = df.iloc[col_name_row].values
+        # Skip the rows that originally contained the column names as they are now stored as the dataframe header
+        df = df.iloc[col_name_row+1:]
+        # reset the index so that the first row is index 0
+        df.reset_index(inplace=True, drop=True)
+
+        # Find the depth column
+        # First search for a column name containing "m" or "cm"
+        candidate_depth_col_names = []
+        for col_name in df.columns:
+            if isinstance(col_name, str):
+                for letter_idx, letter in enumerate(col_name):
+                    if letter.lower() == "m":
+
+                        if (letter_idx == 0) and (col_name[letter_idx+1] in [" ", "]", ")"]) and (col_name not in candidate_depth_col_names):
+                            candidate_depth_col_names.append(col_name)
+                            break
+                        # including "c" when checking preceeding characters to check for "cm"
+                        elif (letter_idx == len(col_name)-1) and (col_name[letter_idx-1] in ["c", " ", "[", "("]) and (col_name not in candidate_depth_col_names):
+                            candidate_depth_col_names.append(col_name)
+                            break
+                        # it's somewhere in the middle
+                        else:
+                            # including "c" when checking preceeding characters to check for "cm"
+                            if (col_name[letter_idx-1] in ["c", " ", "[", "("]) and (col_name[letter_idx+1] in [" ", "]", ")"]) and (col_name not in candidate_depth_col_names):
+                                candidate_depth_col_names.append(col_name)
+                                break
+
+        # The search for "m" identified the depth column
+        if len(candidate_depth_col_names) >= 1:
+            col1 = candidate_depth_col_names[0]
+            df.attrs["candidate_depth_column_names_in_original_file"] = candidate_depth_col_names
+            df.attrs["adopted_depth_column_name_in_original_file"] = col1
+            remaining_cols_to_search = list(df.columns)
+            remaining_cols_to_search.remove(col1)
+
+
+
+
+        # If no columns with "m" or "cm" are found, look for columns with "depth" or "length" or "h "
+        else:
+            col1, df, remaining_cols_to_search = find_col_name_from_substring(df,
+                                                                              ["depth", "length", "h "],
+                                                                              list(df.columns),
+                                                                              "depth")
+
+
+        # Identify the cone resistance, q_c, column
+        col2, df, remaining_cols_to_search = find_col_name_from_substring(df,
+                                                                          [" q ", "qc","q_c", "cone",
+                                                                                     "resistance", "res", "tip"],
+                                                                          remaining_cols_to_search,
+                                                                          "cone_resistance")
+
+        # Identify the sleeve friction, f_s, column
+        col3, df, remaining_cols_to_search = find_col_name_from_substring(df,
+                                                                          ["fs", "sleeve", "friction",
+                                                                                     "local"],
+                                                                          remaining_cols_to_search,
+                                                                          "sleeve_friction")
+        # Identify the porewater pressure, u2, column
+        col4, df, remaining_cols_to_search = find_col_name_from_substring(df,
+                                                                          ["dynamic", "u2", "pore","u",
+                                                                                    "water"],
+                                                                          remaining_cols_to_search,
+                                                                          "porewater_pressure")
+        ################################################
+
+        if (col1 is not None) and (col2 is not None) and (col3 is not None) and (col4 is not None):
+
+            # Return the DataFrame with only the relevant columns
+            df = df[[col1, col2, col3, col4]].rename(
+                columns={col1: "depth_m", col2: "qc_mpa", col3: "fs_mpa", col4: "u_mpa"})
+
+            return df
+
+        else:
+
+            missing_cols = []
+            if col1 is None:
+                missing_cols.append("depth")
+            if col2 is None:
+                missing_cols.append("cone_resistance")
+            if col3 is None:
+                missing_cols.append("sleeve_friction")
+            if col4 is None:
+                missing_cols.append("porewater_pressure")
+
+            missing_cols_per_sheet.append(missing_cols)
+
+            if sheet_idx < len(sheet_names) - 1:
+                # There are more sheets to check so continue onto the next sheet
+                continue
+
+            else:
+                # There are no more sheets to check so return the missing columns
+                final_num_missing_cols = 5
+                final_missing_cols = []
+                for missing_cols in missing_cols_per_sheet:
+                    if len(missing_cols) < final_num_missing_cols:
+                        final_num_missing_cols = len(missing_cols)
+                        final_missing_cols = missing_cols
+
+                raise ValueError(f"Missing columns: {' ;'.join(final_missing_cols)}")

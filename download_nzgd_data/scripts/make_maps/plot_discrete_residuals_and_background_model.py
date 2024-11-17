0f4f24ec90a4cade1e1a1bda9d850244b3f3fd7d
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-11-18 10:44:46 +1300

    working plot of residuals with background model

--- download_nzgd_data/scripts/make_maps/plot_discrete_residuals_and_background_model.py
+++ download_nzgd_data/scripts/make_maps/plot_discrete_residuals_and_background_model.py
@@ -17,12 +17,24 @@ geotiff_file = Path("/home/arr65/data/nzgd/resources/vs30map_data_2023_geotiff/v
 info = pygmt.grdinfo(grid=geotiff_file)
 
 vs30_df = pd.read_csv("/home/arr65/data/nzgd/processed_data/cpt/metadata/vs30_calculations.csv")
-vs30_latlon = vs30_df[["latitude", "longitude"]].to_numpy()
+# drop the column 'Unnamed: 0'
+#vs30_df = vs30_df[(vs30_df["cpt_vs_correlation"] == "andrus_2007") & (vs30_df["vs30_correlation"] == "boore_2011")]
+# drop duplicate rows except for the first occurrence
+vs30_df.drop_duplicates(subset=["cpt_name"],keep='first',inplace=True)
+
+
+vs30_latlon = vs30_df[["latitude","longitude"]].to_numpy()
 
 vs30_nztm = coordinates.wgs_depth_to_nztm(vs30_latlon)
 
-vs30_df["nztm_y"] = vs30_nztm[:, 0]
-vs30_df["nztm_x"] = vs30_nztm[:, 1]
+vs30_df.loc[:,"nztm_y"] = vs30_nztm[:, 0]
+vs30_df.loc[:,"nztm_x"] = vs30_nztm[:, 1]
+
+model_vs30_df = pygmt.grdtrack(points=vs30_df[["nztm_x", "nztm_y"]],
+                            grid=geotiff_file, newcolname="model_vs30")
+
+vs30_df.loc[:,"model_vs30"] = model_vs30_df["model_vs30"]
+vs30_df.loc[:,"ln_cptvs30_minus_modelvs30"] = np.log(vs30_df["vs30"]) - np.log(vs30_df["model_vs30"])
 
 # Step 1: Downsample the GeoTIFF using grdsample to a lower resolution
 downsampled_grid = pygmt.grdsample(
@@ -33,39 +45,30 @@ downsampled_grid = pygmt.grdsample(
 ### All of NZ
 region = [166, 179, -47.5, -34.1]
 
-
-
-# Create a new figure
+# Plot the GeoTIFF in the background
 fig = pygmt.Figure()
+
 
 fig.grdimage(
     grid=downsampled_grid,          # Path to the GeoTIFF file
     cmap="viridis",             # Color map for the data
     frame=True,                  # Add frame to the map
 )
-fig.colorbar(frame='af+lVs30 (m/s)')  # Label the color bar as needed
+fig.colorbar(frame='af+lBackground Vs30 model (m/s)')  # Label the color bar as needed
 
-# Plot the GeoTIFF
-### This doesn't work
-# fig.grdimage(
-#     grid=downsampled_grid,          # Path to the GeoTIFF file
-#     cmap="viridis",             # Color map for the data
-#     frame=True,                  # Add frame to the map
-#     projection="M6i",           # Set the map projection
-#     region=region,               # Set the region to plot
-# )
-
-pygmt.makecpt(cmap="mag", series=[vs30_df["vs30"].min(), vs30_df["vs30"].max()])
+resid_colormin = -1.2
+resid_colormax = 1.0
+pygmt.makecpt(cmap="haxby", series=[resid_colormin, resid_colormax])
 fig.plot(
     x=vs30_df["nztm_x"],
     y=vs30_df["nztm_y"],
-    fill=vs30_df["vs30"],
+    fill=vs30_df["ln_cptvs30_minus_modelvs30"],
     cmap=True,
-    style="c0.05c")
+    style="c0.08c",
+    pen="black")
 
-fig.colorbar(position="JML+o0.5c/0c")
+fig.colorbar(frame="a0.1+lln(CPT Vs30) - ln(Model Vs30) (discrete points)",position="JML+o2c/0c")
 fig.savefig(Path("/home/arr65/data/nzgd/plots") / "vs30_from_geotiff.png", dpi=500)
-
 
 # Show the plot
 #fig.show()

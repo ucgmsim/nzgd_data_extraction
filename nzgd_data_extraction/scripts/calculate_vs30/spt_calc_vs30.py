390f6a0320a5546e18c12b868f82b409d3e12af5
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-11-27 08:15:20 +1300

    added VsViewer to path

--- nzgd_data_extraction/scripts/calculate_vs30/spt_calc_vs30.py
+++ nzgd_data_extraction/scripts/calculate_vs30/spt_calc_vs30.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import sys
+import numpy as np
+from pathlib import Path
 
-df = pd.read_parquet("/home/arr65/data/nzgd/processed_data/spt/spts.parquet")
+vs_calc_path = Path("/home/arr65/src/Vs30/VsViewer")
+sys.path.append(str(vs_calc_path))
+
+#df = pd.read_parquet("/home/arr65/data/nzgd/processed_data/spt/spts.parquet")
 
 print()

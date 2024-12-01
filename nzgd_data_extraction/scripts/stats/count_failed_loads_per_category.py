b8c456bc4368731770e1d2fa6af247d5ba251e04
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-12-02 08:36:25 +1300

    count number of unknown errors

--- nzgd_data_extraction/scripts/stats/count_failed_loads_per_category.py
+++ nzgd_data_extraction/scripts/stats/count_failed_loads_per_category.py
@@ -4,5 +4,6 @@ df = pd.read_csv("/home/arr65/data/nzgd/processed_data/cpt/metadata/all_failed_l
 
 num_depth_is_index = df[df["category"] == "depth_is_index"].shape[0]
 
+num_unknown_category_errors = df[df["category"] == "unknown_category"].shape[0]
 
 print()

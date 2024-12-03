9163a4efee440119f750ab7159415ee76c200d55
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-12-04 11:21:46 +1300

    process cpt and scpt in with a loop

--- nzgd_data_extraction/scripts/process/process_cpt_data_script.py
+++ nzgd_data_extraction/scripts/process/process_cpt_data_script.py
@@ -14,90 +14,93 @@ from nzgd_data_extraction.lib import process_cpt_data, processing_helpers
 
 if __name__ == "__main__":
 
-    investigation_type = processing_helpers.InvestigationType.cpt
+    #investigation_type = processing_helpers.InvestigationType.cpt
+    investigation_type = processing_helpers.InvestigationType.scpt
 
-    nzgd_index_df = pd.read_csv(Path("/home/arr65/data/nzgd/resources/nzgd_index_files/csv_files/"
-                                     "NZGD_Investigation_Report_08112024_1017.csv"))
+    for investigation_type in [processing_helpers.InvestigationType.cpt, processing_helpers.InvestigationType.scpt]:
 
-    output_dir = Path(f"/home/arr65/data/nzgd/test_processed_data/{investigation_type}")
+        nzgd_index_df = pd.read_csv(Path("/home/arr65/data/nzgd/resources/nzgd_index_files/csv_files/"
+                                         "NZGD_Investigation_Report_08112024_1017.csv"))
 
-    if output_dir.exists():
-        raise ValueError("Output directory already exists. Delete or rename previous output and try again.")
+        output_dir = Path(f"/home/arr65/data/nzgd/processed_data/{investigation_type}")
 
-    parquet_output_path = output_dir / "data"
-    metadata_output_dir = output_dir / "metadata"
+        if output_dir.exists():
+            raise ValueError("Output directory already exists. Delete or rename previous output and try again.")
 
-    parquet_output_path.mkdir(exist_ok=True, parents=True)
-    metadata_output_dir.mkdir(exist_ok=True, parents=True)
+        parquet_output_path = output_dir / "data"
+        metadata_output_dir = output_dir / "metadata"
 
-    downloaded_files = Path(f"/home/arr65/data/nzgd/downloads_and_metadata/unorganised_raw_from_nzgd/{investigation_type}")
+        parquet_output_path.mkdir(exist_ok=True, parents=True)
+        metadata_output_dir.mkdir(exist_ok=True, parents=True)
 
-    ## If records should be skipped, enter them here
-    records_to_skip = []
+        downloaded_files = Path(f"/home/arr65/data/nzgd/downloads_and_metadata/unorganised_raw_from_nzgd/{investigation_type}")
 
-    records_to_process = []
-    for record_dir in natsort.natsorted(list(downloaded_files.glob("*"))):
-        if record_dir.name not in records_to_skip:
-            records_to_process.append(record_dir)
+        ## If records should be skipped, enter them here
+        records_to_skip = []
 
-    downloaded_record_names = set([record_dir.name for record_dir in records_to_process])
+        records_to_process = []
+        for record_dir in natsort.natsorted(list(downloaded_files.glob("*"))):
+            if record_dir.name not in records_to_skip:
+                records_to_process.append(record_dir)
 
-    ## A small number of records have been removed from the NZGD after they were downloaded.
-    ## These records were likely removed for a reason such data quality or permission issues, so they are not considered.
-    records_currently_in_nzgd = set(nzgd_index_df["ID"].values)
-    records_that_have_been_removed = downloaded_record_names - records_currently_in_nzgd
+        downloaded_record_names = set([record_dir.name for record_dir in records_to_process])
 
-    if len(records_that_have_been_removed) > 0:
-        print("The following records have been removed from the NZGD and will not be processed:")
-        for removed_record in records_that_have_been_removed:
-            print(removed_record)
+        ## A small number of records have been removed from the NZGD after they were downloaded.
+        ## These records were likely removed for a reason such data quality or permission issues, so they are not considered.
+        records_currently_in_nzgd = set(nzgd_index_df["ID"].values)
+        records_that_have_been_removed = downloaded_record_names - records_currently_in_nzgd
 
-        ## Remove the records that have been removed from the list of records to process
-        records_to_process = [record_dir for record_dir in records_to_process if record_dir.name not in records_that_have_been_removed]
+        if len(records_that_have_been_removed) > 0:
+            print("The following records have been removed from the NZGD and will not be processed:")
+            for removed_record in records_that_have_been_removed:
+                print(removed_record)
 
-    process_one_record_partial = functools.partial(process_cpt_data.process_one_record,
-                                                   parquet_output_dir=parquet_output_path,
-                                                   nzgd_index_df=nzgd_index_df,
-                                                   investigation_type=investigation_type)
-    results = []
-    num_workers = 8
-    with mp.Pool(processes=num_workers) as pool:
-        results.extend(list(tqdm(pool.imap(process_one_record_partial, records_to_process),
-                                 total=len(records_to_process))))
+            ## Remove the records that have been removed from the list of records to process
+            records_to_process = [record_dir for record_dir in records_to_process if record_dir.name not in records_that_have_been_removed]
+
+        process_one_record_partial = functools.partial(process_cpt_data.process_one_record,
+                                                       parquet_output_dir=parquet_output_path,
+                                                       nzgd_index_df=nzgd_index_df,
+                                                       investigation_type=investigation_type)
+        results = []
+        num_workers = 8
+        with mp.Pool(processes=num_workers) as pool:
+            results.extend(list(tqdm(pool.imap(process_one_record_partial, records_to_process),
+                                     total=len(records_to_process))))
 
 
-    ### concatenate all the metadata dataframes
-    print("Concatenating the metadata dataframes")
+        ### concatenate all the metadata dataframes
+        print("Concatenating the metadata dataframes")
 
-    spreadsheet_format_descriptions_dfs = []
-    all_failed_loads_dfs = []
-    loading_summary_dfs = []
+        spreadsheet_format_descriptions_dfs = []
+        all_failed_loads_dfs = []
+        loading_summary_dfs = []
 
-    for result in tqdm(results):
-        if result.spreadsheet_format_description_per_record is not None:
-            spreadsheet_format_descriptions_dfs.append(result.spreadsheet_format_description_per_record)
-            all_failed_loads_dfs.append(result.all_failed_loads_df)
-            loading_summary_dfs.append(result.loading_summary_df)
+        for result in tqdm(results):
+            if result.spreadsheet_format_description_per_record is not None:
+                spreadsheet_format_descriptions_dfs.append(result.spreadsheet_format_description_per_record)
+                all_failed_loads_dfs.append(result.all_failed_loads_df)
+                loading_summary_dfs.append(result.loading_summary_df)
 
-    spreadsheet_format_descriptions_df = pd.concat(spreadsheet_format_descriptions_dfs,
-                                                     ignore_index=True)
+        spreadsheet_format_descriptions_df = pd.concat(spreadsheet_format_descriptions_dfs,
+                                                         ignore_index=True)
 
-    all_failed_loads_df = pd.concat(all_failed_loads_dfs,
-                                                     ignore_index=True)
+        all_failed_loads_df = pd.concat(all_failed_loads_dfs,
+                                                         ignore_index=True)
 
-    loading_summary_df = pd.concat(loading_summary_dfs,
-                                                     ignore_index=True)
+        loading_summary_df = pd.concat(loading_summary_dfs,
+                                                         ignore_index=True)
 
-    spreadsheet_format_descriptions_df.to_csv(metadata_output_dir / "spreadsheet_format_description.csv", index=False)
-    all_failed_loads_df.to_csv(metadata_output_dir / "all_failed_loads.csv", index=False)
-    loading_summary_df.to_csv(metadata_output_dir / "loading_summary.csv", index=False)
+        spreadsheet_format_descriptions_df.to_csv(metadata_output_dir / "spreadsheet_format_description.csv", index=False)
+        all_failed_loads_df.to_csv(metadata_output_dir / "all_failed_loads.csv", index=False)
+        loading_summary_df.to_csv(metadata_output_dir / "loading_summary.csv", index=False)
 
-    if len(spreadsheet_format_descriptions_df) > 0:
+        if len(spreadsheet_format_descriptions_df) > 0:
 
-        unique_spreadsheet_format_descriptions_df = spreadsheet_format_descriptions_df.drop(columns=["header_row_index", "record_name", "file_name"])
+            unique_spreadsheet_format_descriptions_df = spreadsheet_format_descriptions_df.drop(columns=["header_row_index", "record_name", "file_name"])
 
-        # get number of unique rows in spreadsheet_format_description_df
-        unique_spreadsheet_format_descriptions_df = unique_spreadsheet_format_descriptions_df.drop_duplicates()
-        unique_spreadsheet_format_descriptions_df.to_csv(metadata_output_dir / "spreadsheet_format_description_unique.csv",
-                                                        index=False)
+            # get number of unique rows in spreadsheet_format_description_df
+            unique_spreadsheet_format_descriptions_df = unique_spreadsheet_format_descriptions_df.drop_duplicates()
+            unique_spreadsheet_format_descriptions_df.to_csv(metadata_output_dir / "spreadsheet_format_description_unique.csv",
+                                                            index=False)
 

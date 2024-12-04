6d845526b23b964fbee90cea1680d7cb4df3614f
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-12-05 11:19:35 +1300

    finished website database file

--- nzgd_data_extraction/scripts/organise/assemble_flat_files_for_website_database.py
+++ nzgd_data_extraction/scripts/organise/assemble_flat_files_for_website_database.py
@@ -14,8 +14,10 @@ from nzgd_data_extraction.lib import organise
 nzgd_df = pd.read_csv(
     "/home/arr65/data/nzgd/resources/nzgd_index_files/csv_files/NZGD_Investigation_Report_08112024_1017.csv"
 )
-### drop columns X and Y
+
+nzgd_df = nzgd_df[(nzgd_df["Type"] == "Borehole") | (nzgd_df["Type"] == "CPT") | (nzgd_df["Type"] == "SCPT")]
 nzgd_df.drop(columns=["X", "Y", "Coordinate System","URL"], inplace=True)
+
 nzgd_df.rename(
     columns={
         "ID": "record_name",
@@ -33,7 +35,8 @@ nzgd_df.rename(
 region_df = pd.read_csv(
     "/home/arr65/data/nzgd/resources/metadata_from_nzgd_location/regions_NZGD_Investigation_Report_08112024_1017.csv"
 )
-
+## Drop velocity profiles (VsVp)
+region_df = region_df[~region_df["record_id"].str.contains("VsVp")]
 region_df.rename(
     columns={
         "record_id": "record_name",
@@ -44,33 +47,8 @@ region_df.rename(
     },
     inplace=True,
 )
-
 region_df = region_df[["record_name", "region", "district", "city", "suburb"]]
 region_df = region_df.map(organise.replace_chars)
-
-#####################################################################
-file_path_df = pd.read_csv(
-    "/home/arr65/data/nzgd/resources/metadata_from_nzgd_location/file_names_linked_to_regions_NZGD_Investigation_Report_08112024_1017.csv"
-)
-links_df_for_merge = region_df[["record_name"]].copy()
-links_df_for_merge["raw_files"] = None
-links_df_for_merge["processed_files"] = None
-
-links_df_for_merge.set_index("record_name", inplace=True)
-
-for idx, row in tqdm(links_df_for_merge.iterrows()):
-
-
-    raw_file_links = file_path_df[(file_path_df["record_name"] == idx) &
-                                  (file_path_df["file_type"] == "raw")]["link"].to_list()
-
-    processed_file_links = file_path_df[(file_path_df["record_name"] == idx) &
-                                  (file_path_df["file_type"] == "processed")]["link"].to_list()
-
-    links_df_for_merge.at[idx, "raw_files"] = raw_file_links
-    links_df_for_merge.at[idx, "processed_files"] = processed_file_links
-
-#####################################################################
 
 foster_vs30_df = pd.read_csv(
     "/home/arr65/data/nzgd/resources/metadata_from_nzgd_location/foster_vs30_at_nzgd_locations_NZGD_Investigation_Report_08112024_1017.csv"
@@ -83,6 +61,7 @@ foster_vs30_df.rename(
     },
     inplace=True,
 )
+foster_vs30_df = foster_vs30_df[foster_vs30_df["record_name"].isin(nzgd_df["record_name"])]
 
 spt_vs30_df = pd.read_csv("/home/arr65/data/nzgd/processed_data/spt/spt_vs30.csv")
 scpt_vs30_df = pd.read_csv("/home/arr65/data/nzgd/processed_data/scpt/metadata/vs30_estimates_from_cpt.csv")
@@ -96,19 +75,18 @@ vs30_df = pd.concat([spt_vs30_df, scpt_vs30_df, cpt_vs30_df], ignore_index=True)
 # )
 
 ### Merge DataFrames using the record_name column
-merged_df = pd.merge(nzgd_df, region_df, on="record_name")
-merged_df = pd.merge(merged_df, foster_vs30_df, on="record_name")
-merged_df = pd.merge(merged_df, spt_vs30_df, on="record_name")
+df_from_record_locations = pd.merge(nzgd_df, region_df, on="record_name")
+df_from_record_locations = pd.merge(df_from_record_locations, foster_vs30_df, on="record_name")
+
+merged_df = pd.merge(df_from_record_locations, vs30_df, on="record_name", how="outer")
 
 merged_df["vs30_log_residual"] = np.log(merged_df["vs30"]) - np.log(
     merged_df["foster_2019_vs30"]
 )
 
-merged_df["raw_files"] = np.nan
-merged_df["processed_files"] = np.nan
+merged_df.to_csv("/home/arr65/data/nzgd/resources/website_database.csv")
 
-merged_df.set_index("record_name", inplace=True)
 
 merged_df.to_parquet(
-    Path("/home/arr65/src/nzgd_map_from_webplate/instance/spt_vs30.parquet")
+    Path("/home/arr65/src/nzgd_map_from_webplate/instance/website_database.parquet")
 )

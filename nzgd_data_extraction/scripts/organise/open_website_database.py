06a39e0fefc207e565ddcded6cddfd1d5813d804
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-12-06 14:07:13 +1300

    useful script for debugging the flask app

--- nzgd_data_extraction/scripts/organise/open_website_database.py
+++ nzgd_data_extraction/scripts/organise/open_website_database.py
@@ -1,23 +1,19 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_parquet("/home/arr65/src/nzgd_map_from_webplate/instance/website_database.parquet")
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
-vs30_correlation = "boore_2011"
-spt_vs_correlation = "brandenberg_2010"
-cpt_vs_correlation = "andrus_2007_pleistocene"
+record_name = "CPT_136028"
+cpt_df = pd.read_parquet("/home/arr65/src/nzgd_map_from_webplate/instance/extracted_cpt_and_scpt_data.parquet",
+                             filters=[("record_name", "==", record_name)]).reset_index()
 
-# Filter the dataframe for the selected spt_vs_correlation_and_vs30_correlation
-df = df[df["vs30_correlation"] == vs30_correlation]
+fig = make_subplots(rows=1, cols=3)
 
-spt_bool = (df["spt_vs_correlation"] == spt_vs_correlation) & (df["spt_hammer_type"] == "Auto")
-cpt_bool = df["cpt_vs_correlation"] == cpt_vs_correlation
+fig.add_trace(go.Scatter(x=cpt_df["qc"], y=cpt_df["Depth"]), row=1, col=1)
+fig.add_trace(go.Scatter(x=cpt_df["fs"], y=cpt_df["Depth"]), row=1, col=2)
+fig.add_trace(go.Scatter(x=cpt_df["u"], y=cpt_df["Depth"]), row=1, col=3)
 
-df = df[spt_bool | cpt_bool]
-
-num_records = df["record_name"].nunique()
-
-#df.drop_duplicates(subset=["record_name"], keep="first", inplace=True)
-
-
-
-print()
+fig.update_yaxes(title_text="qc (Mpa)", row=1, col=1)
+fig.update_yaxes(title_text="fs (Mpa)", row=1, col=2)
+fig.update_yaxes(title_text="u2 (Mpa)", row=1, col=2)

f126868869edc6894bf3f7305b866dd00d8f8189
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-12-13 16:45:17 +1300

    basic working prototype with correlations from given semivariogram

--- nzgd_data_extraction/scripts/robust_vs30_uncertainty/correlated_velocity_profiles.py
+++ nzgd_data_extraction/scripts/robust_vs30_uncertainty/correlated_velocity_profiles.py
@@ -18,16 +18,24 @@ sys.path.append(str(vs_calc_path))
 import vs_calc
 
 
-def compute_partial_correlation(depth, vs, vs_sd, n_to_sample) -> tuple[npt.NDArray, npt.NDArray]:
+def claire_dong_compute_partial_correlation(depth, vs, vs_sd, num_to_sample) -> tuple[npt.NDArray, npt.NDArray]:
     # Pick a value for constant a
-    rho_0 = 0.02
-    h_0 = 5
-    a = np.log(rho_0) / (-1 * h_0)
+    # rho_0 = 0.02
+    # h_0 = 5
+    # a = np.log(rho_0) / (-1 * h_0)
     # Find the lag distance between each pair of values
     h = abs(np.subtract.outer(depth, depth))
     # Form the correlation rho, which is covariance divided by standard deviation
     # (in the range from 0 to 1)
-    rho = np.exp(-1 * a * h)
+    #rho = np.exp(-1 * a * h)
+    ######################################################
+    ### Andrew's rho calculation
+    sill = 0.8
+    range_m = 3
+    gamma = sill * (1 - np.exp(-3 * h / range_m))
+    rho = sill - gamma
+    ################################################
+
     # Form the covariance matrix (population standDev * rho * population standDev)
     sd = np.diag(vs_sd.reshape(-1))
     cov = np.dot(sd, np.dot(rho, sd))
@@ -35,8 +43,8 @@ def compute_partial_correlation(depth, vs, vs_sd, n_to_sample) -> tuple[npt.NDAr
     cholesky_cov = np.linalg.cholesky(cov)
     
     # Generate random uncorrelated values with depth~(0,1)
-    uncorrelated_random_sample = norm.rvs(loc=0, scale=1, size=((len(depth)), n_to_sample))
-    
+    uncorrelated_random_sample = norm.rvs(loc=0, scale=1, size=((len(depth)), num_to_sample))
+
     # Multiply by cholesky(cov) to generate random correlated values
     random_correlated_variables = cholesky_cov @ uncorrelated_random_sample
     # Adding the means to the random correlated variables
@@ -47,7 +55,64 @@ def compute_partial_correlation(depth, vs, vs_sd, n_to_sample) -> tuple[npt.NDAr
 
     return random_vs, sigma
 
-def sample_vs_profiles_from_distribution(vs_df: pd.DataFrame, n_to_sample: int = 50, correlation_flag="partial"):
+
+def calculate_semivariogram_model_gamma_h(h: npt.NDArray, sill: float, range_m: float) -> npt.NDArray:
+    """
+    Calculate the semivariogram model gamma(h) for a given separation distance h, range r, and sill s.
+
+    Parameters
+    ----------
+    h : npt.NDArray
+        The separation distance h.
+    range_m : float
+        The range parameter, r, is the distance at which the semivariogram, gamma(h), is equal to 95% of the sill
+        (i.e., the distance at which 95% of the correlation is lost).
+    sill : float
+        The sill parameter, s, is equal to the variance of Z = delta W  (the residual of the within event term for ground motions)
+
+    Returns
+    -------
+    npt.NDArray
+        The semivariogram model gamma(h).
+    """
+
+    return sill * (1 - np.exp(-3 * h / range_m))
+
+
+def andrew_compute_partial_correlation(depth, vs, vs_sd, num_to_sample) -> tuple[npt.NDArray, npt.NDArray]:
+
+    ### Defining rho from semivariogram_model ###
+    # Find the lag distance between each pair of values
+    h = abs(np.subtract.outer(depth, depth))
+    sill = 0.8
+    gamma = calculate_semivariogram_model_gamma_h(h=h, sill=sill, range_m=3)
+    covariance = sill - gamma
+    rho = covariance / sill
+
+    ### The rest of the code is the same as Claire Dong's
+    #################################
+
+    # Form the covariance matrix (population standDev * rho * population standDev)
+    sd = np.diag(vs_sd.reshape(-1))
+    cov = np.dot(sd, np.dot(rho, sd))
+    # Cholesky Decomposition to get lower triangular matrix
+    cholesky_cov = np.linalg.cholesky(cov)
+
+    # Generate random uncorrelated values with depth~(0,1)
+    uncorrelated_random_sample = norm.rvs(loc=0, scale=1, size=((len(depth)), num_to_sample))
+
+    # Multiply by cholesky(cov) to generate random correlated values
+    random_correlated_variables = cholesky_cov @ uncorrelated_random_sample
+    # Adding the means to the random correlated variables
+    random_ln_vs = (np.log(vs).reshape(len(vs), 1) + random_correlated_variables)
+    # Return generated samples and log standard deviation
+    random_vs = np.exp(random_ln_vs)
+    sigma = np.std(random_ln_vs)
+
+    return random_vs, sigma
+
+
+def sample_vs_profiles_from_distribution(vs_df: pd.DataFrame, num_to_sample: int = 50):
     
     """
     Sample vs_df["Vs"] profiles from the provided mean and standard deviation at each depth level
@@ -55,7 +120,7 @@ def sample_vs_profiles_from_distribution(vs_df: pd.DataFrame, n_to_sample: int =
     Parameters
     ----------
     vs_df : pd.DataFrame
-    n_to_sample
+    num_to_sample
 
     Returns
     -------
@@ -73,29 +138,22 @@ def sample_vs_profiles_from_distribution(vs_df: pd.DataFrame, n_to_sample: int =
 
     # -----------------Compute random selected vs_df["Vs"]---------------------
 
-    if correlation_flag == 0:
-        ln_vs = np.log(vs_df["Vs"])
-        randomly_sampled_ln_vs = norm.rvs(loc=ln_vs, scale=vs_df["Vs_SD"], size=(len(vs_df["Vs"]), n_to_sample))
-        sigma = np.std(randomly_sampled_ln_vs)
-        randomly_sampled_vs = np.exp(randomly_sampled_ln_vs)
 
-    elif correlation_flag == 1:
-        # Generate the first vs_df["Vs"] and applt its variance to the rest
-        ln_vs = np.log(vs_df["Vs"])
-        randomly_sampled_ln_vs = norm.rvs(loc=ln_vs[0], scale=vs_df["Vs_SD"][0], size=n_to_sample)
-        z_score = (randomly_sampled_ln_vs - ln_vs[0]) / vs_df["Vs_SD"][0]
-        randomly_sampled_ln_vs = vs_df["Vs_SD"] * z_score + ln_vs
-        randomly_sampled_vs = np.exp(randomly_sampled_ln_vs)
-        sigma = np.std(randomly_sampled_ln_vs)
+    # ln_vs = np.log(vs_df["Vs"].values)
+    # vs_sd = vs_df["Vs_SD"].values
 
-    else:
-        randomly_sampled_vs, sigma = compute_partial_correlation(vs_df["Depth"].values, vs_df["Vs"].values, vs_df["Vs_SD"].values, n_to_sample)
+    randomly_sampled_vs, sigma = andrew_compute_partial_correlation(vs_df["Depth"].values, vs_df["Vs"].values, vs_df["Vs_SD"].values, num_to_sample)
+    #randomly_sampled_vs, sigma = claire_dong_compute_partial_correlation(vs_df["Depth"].values, vs_df["Vs"].values, vs_df["Vs_SD"].values, num_to_sample)
+
 
     return randomly_sampled_vs, sigma
 
 
-def calc_vs30_from_filename(file_path: Path, cpt_vs_correlation: str, vs30_correlation: str,
-                            randomly_sampled_vs_profiles_output_dir: Path = None) -> pd.DataFrame:
+def calc_vs30_from_filename(file_path: Path, cpt_vs_correlation: str,
+                            vs30_correlation: str,
+                            num_to_sample = 10,
+                            vs30_per_record_dir : Path = None,
+                            randomly_sampled_vs_profiles_per_record_dir: Path = None) -> pd.DataFrame:
     """
     Calculate Vs30 from a given CPT file.
 
@@ -127,8 +185,6 @@ def calc_vs30_from_filename(file_path: Path, cpt_vs_correlation: str, vs30_corre
     cpt_df = cpt_df_repeat_measures[cpt_df_repeat_measures["investigation_number"] == investigation_number]
     cpt_df = cpt_df[cpt_df["fs"] > 0]
 
-    n_to_sample = 10
-
     try:
         ## Raise an error if there are no valid measurements
         if cpt_df.size == 0:
@@ -148,15 +204,20 @@ def calc_vs30_from_filename(file_path: Path, cpt_vs_correlation: str, vs30_corre
 
         vs_df = cpt_vs_profile.to_dataframe()
 
+        randomly_sampled_vs_profiles, sigma = sample_vs_profiles_from_distribution(vs_df, num_to_sample=num_to_sample)
 
-        randomly_sampled_vs_profiles, sigma = sample_vs_profiles_from_distribution(vs_df, n_to_sample=n_to_sample, correlation_flag="partial")
-        if randomly_sampled_vs_profiles_output_dir:
-            np.save(randomly_sampled_vs_profiles_output_dir / f"{file_path.stem}_random_vs_profiles.npy",
-                    randomly_sampled_vs_profiles)
+        if randomly_sampled_vs_profiles_per_record_dir:
+            vs_profiles_df = pd.DataFrame(columns=["record_name", "depth", "vs", "vs_sigma"], index=[0], dtype="object")
+            vs_profiles_df.at[0, "record_name"] = cpt_df["record_name"].values[0]
+            vs_profiles_df.at[0, "depth"] = vs_df["Depth"].values
+            vs_profiles_df.at[0, "num_sampled_profiles"] = num_to_sample
+            vs_profiles_df.at[0, "vs"] = randomly_sampled_vs_profiles.tolist()
+            vs_profiles_df.at[0, "vs_sigma"] = sigma
 
+            vs_profiles_df.to_parquet(randomly_sampled_vs_profiles_per_record_dir / f"{file_path.stem}_random_vs_profiles.parquet")
 
-        vs30_from_sampled_profiles = np.zeros(n_to_sample)
-        for vs_profile_idx in range(n_to_sample):
+        vs30_from_sampled_profiles = np.zeros(num_to_sample)
+        for vs_profile_idx in range(num_to_sample):
             vs_profile_for_idx = vs_calc.VsProfile(name=file_path.stem,
                                                    depth=vs_df["Depth"].values,
                                                    vs=randomly_sampled_vs_profiles[:, vs_profile_idx],
@@ -170,14 +231,13 @@ def calc_vs30_from_filename(file_path: Path, cpt_vs_correlation: str, vs30_corre
         vs30_std = np.std(vs30_from_sampled_profiles)
         error = np.nan
 
-
     except Exception as e:
         # Handle any exceptions by setting Vs30 and its standard deviation to NaN
         vs30 = np.nan
         vs30_std = np.nan
-        error = e
+        error = str(e)
 
-    # Create a DataFrame with the Vs30 calculation results and metadata
+    ### Create a DataFrame with the Vs30 calculation results and metadata
     cpt_vs30_df = pd.DataFrame(
                  {"record_name": cpt_df["record_name"].values[0],
                   "record_type": "cpt",
@@ -188,7 +248,7 @@ def calc_vs30_from_filename(file_path: Path, cpt_vs_correlation: str, vs30_corre
                   "num_depth_levels": cpt_df["Depth"].size,
                   "vs30": vs30,
                   "vs30_std": vs30_std,
-                  "num_sampled_vs_profiles": n_to_sample,
+                  "num_sampled_vs_profiles": num_to_sample,
                   "vs30_correlation": vs30_correlation,
                   "cpt_vs_correlation": cpt_vs_correlation,
                   "spt_vs_correlation": np.nan,
@@ -197,7 +257,8 @@ def calc_vs30_from_filename(file_path: Path, cpt_vs_correlation: str, vs30_corre
                   "spt_borehole_diameter": np.nan},
                  index=[0])
 
-    cpt_vs30_df.to_parquet(randomly_sampled_vs_profiles_output_dir / f"{file_path.stem}_vs30.parquet")
+    if vs30_per_record_dir:
+        cpt_vs30_df.to_parquet(vs30_per_record_dir / f"{file_path.stem}_vs30.parquet")
 
     return cpt_vs30_df
 
@@ -212,17 +273,20 @@ if __name__ == "__main__":
         ## value warnings that are suppressed.
         np.seterr(divide='ignore', invalid='ignore')
 
-        #parquet_dir = Path(f"/home/arr65/data/nzgd/processed_data/{investigation_type}/data")
-        parquet_dir = Path(f"/home/arr65/data/nzgd/processed_data/{investigation_type}/extracted_data_per_record")
-        vs_profiles_dir = Path("/home/arr65/data/nzgd/robust_vs30/randomly_sampled_velocity_profiles")
+        num_to_sample = 1000
 
-        vs30_output_dir = Path("/home/arr65/data/nzgd/robust_vs30/vs30_estimates")
+        #cpt_data_dir = Path(f"/home/arr65/data/nzgd/processed_data/{investigation_type}/data")
+        cpt_data_dir = Path(f"/home/arr65/data/nzgd/processed_data/{investigation_type}/extracted_data_per_record")
+
+        vs_profiles_dir = Path(f"/home/arr65/data/nzgd/robust_vs30/{investigation_type}/randomly_sampled_velocity_profiles")
+        vs30_output_dir = Path(f"/home/arr65/data/nzgd/robust_vs30/{investigation_type}/vs30_estimate_per_record")
         
         vs_profiles_dir.mkdir(exist_ok=True, parents=True)
         vs30_output_dir.mkdir(exist_ok=True, parents=True)
 
-        file_paths = natsort.natsorted(list(parquet_dir.glob("*.parquet")))
-        file_paths = file_paths[:10]
+        file_paths = natsort.natsorted(list(cpt_data_dir.glob("*.parquet")))
+        #file_paths = file_paths[4:6]
+        file_paths = file_paths[0:2]
 
         # cpt_vs_correlations = ["andrus_2007_pleistocene","andrus_2007_holocene", "andrus_2007_tertiary_age_cooper_marl",
         #                        "robertson_2009","hegazy_2006","mcgann_2015","mcgann_2018"]
@@ -230,7 +294,7 @@ if __name__ == "__main__":
         cpt_vs_correlations = ["mcgann_2015"]
 
         #vs30_correlations = ["boore_2004", "boore_2011"]
-        vs30_correlations = ["boore_2011"]
+        vs30_correlations = ["boore_2004"]
 
         # cpt_vs_correlations = ["andrus_2007_pleistocene", "robertson_2009", "mcgann_2018"]
         # vs30_correlations = ["boore_2004", "boore_2011"]
@@ -242,15 +306,24 @@ if __name__ == "__main__":
                 description_text = f"Calculating Vs30 using {vs30_correlation} and {cpt_vs_correlation}"
                 print(description_text)
 
+                # calc_vs30_from_filename_partial = functools.partial(calc_vs30_from_filename,
+                #                                           cpt_vs_correlation=cpt_vs_correlation,
+                #                                           vs30_correlation=vs30_correlation,
+                #                                           num_to_sample = num_to_sample,
+                #                                           vs30_per_record_dir = vs30_output_dir,
+                #                                           randomly_sampled_vs_profiles_per_record_dir=vs_profiles_dir)
+
                 calc_vs30_from_filename_partial = functools.partial(calc_vs30_from_filename,
                                                           cpt_vs_correlation=cpt_vs_correlation,
                                                           vs30_correlation=vs30_correlation,
-                                                          randomly_sampled_vs_profiles_output_dir=vs_profiles_dir)
+                                                          num_to_sample = num_to_sample,
+                                                          vs30_per_record_dir = vs30_output_dir,
+                                                          randomly_sampled_vs_profiles_per_record_dir=None)
                 num_workers = 8
                 with mp.Pool(processes=num_workers) as pool:
                     results.extend(list(tqdm(pool.imap(calc_vs30_from_filename_partial, file_paths),
                                         total=len(file_paths))))
 
-        pd.concat(results, ignore_index=True).to_csv(vs30_output_dir / f"vs30_estimates_from_cpt.csv", index=False)
+        pd.concat(results, ignore_index=True).to_csv(vs30_output_dir.parent / f"vs30_estimates_from_cpt.csv", index=False)
 
     print(f"Total time taken: {(time.time() - start_time)/3600} hours")
